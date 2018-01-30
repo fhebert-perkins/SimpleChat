a9ae8ff3cda7b8e70f0111e4d374135d3591b9d3
finley hebert-perkins <finley.hebert@gmail.com>
2018-01-29 20:05:31 -0800

    python 3 rewrite

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-#! /bin/python2
+#! /bin/python3
 
 # The MIT License (MIT)
 #
-# Copyright (c) 2016 Finley Hebert-Perkins
+# Copyright (c) 2018 Finley Hebert-Perkins
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,137 +22,96 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from twisted.internet   import reactor, protocol, endpoints
-from twisted.protocols  import basic
-import config as cfg
+import asyncio
 
-class ChatProtocol(basic.LineReceiver):
-    def __init__(self, factory):
-        self.factory = factory
-        self.name = "Guest{}".format(len(self.factory.channels[cfg.defaultChannel])+1)
-        self.channel = cfg.defaultChannel
-        self.away = False
+class Peer:
+    def __init__(self, transport):
+        self.transport = transport
+        self.name = "guest{}".format(len(peers))
 
-    def broadcast(self, message, noEcho=True):
-        for c in self.factory.channels[self.channel]:
-            if c.name != self.name:
-                c.sendLine(message)
-            elif noEcho:
-                pass
-            else:
-                c.sendLine(message)
+    def set_name(self, name):
+        self.name = name
 
-    def connectionMade(self): # On a new connection
-        self.factory.channels[cfg.defaultChannel].add(self) # add client to client set
-        self.sendLine("! Use /name <name> to rename yourself") # tell user how to change name
-        self.broadcast("! {} has joined".format(self.name), noEcho=False) # tell all those who are connected that someone has connected
+peers = []
 
-    def connectionLost(self, reason):
-        self.broadcast("! {} has left".format(self.name), noEcho = False) # tell everyone that person has left
-        self.factory.channels[self.channel].remove(self) # remove from set
+class ChatServerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(self.peername))
+        self.peer = Peer(transport)
+        self.transport = transport
+        peers.append(self.peer)
+        self.broadcast("! {} has joined".format(self.peer.name))
 
-    def lineReceived(self, line):
-        try:
-            line = line.decode("utf-8") # turn the bits sent into a string
-        except:
-            self.sendLine("! Malformed string sent")
-            return 0
-        if line.startswith("/name"): # name command
-            if len(line.split(" ")) >= 2 :
-                names = []
-                for channel in self.factory.channels:
-                    for c in self.factory.channels[channel]:
-                        names.append(c.name)
-                if line.split(" ")[1] not in names:
-                    self.broadcast("! {} is now {}".format(self.name, line.split(" ")[1]))
-                    self.name = line.split(" ")[1].encode("utf-8")
-                    self.factory.channels[self.channel].remove(self)
-                    self.factory.channels[self.channel].add(self)
+    def echo(self, message):
+        print("echo: {}".format(message))
+        message = message+"\n"
+        self.transport.write(message.encode("UTF-8"))
+
+    def broadcast(self, message):
+        print("broadcast: {}".format(message))
+        message = message+"\n"
+        for peer in peers:
+            peer.transport.write(message.encode("UTF-8"))
+
+    def data_received(self, data):
+        message = data.decode().strip()
+        # command handling
+        if message.startswith('/'):
+            message = message[1:]
+            if message.startswith("list"):
+                self.echo(",".join([peer.name for peer in peers]))
+            elif message.startswith("name"):
+                tokens = message.split(" ")
+                if len(tokens) == 2:
+                    for peer in peers:
+                        if peer.name == tokens[1]:
+                            self.echo("! unable to set name to {} because that name is already in use".format(tokens[1]))
+                            return
+                    self.broadcast("! {} changed thier name to {}".format(self.peer.name, tokens[1]))
+                    self.peer.set_name(tokens[1])
+                    return
                 else:
-                    self.sendLine("! That name is already in use")
-            else:
-                self.sendLine("! Command malformed")
-
-        elif line.startswith("/whoami"):
-            self.sendLine("! Your are {}".format(self.name))
-
-        elif line.startswith("/me"): # me command
-            self.broadcast(line.replace("/me", "*{}".format(self.name)), noEcho=False)
-
-        elif line.startswith("/list"): # list all people connected
-            if len(line.split(" ")) == 1:
-                self.sendLine(", ".join([c.name for c in self.factory.channels[self.channel]]).encode("utf-8"))
-            elif line.split(" ")[1] == "list":
-                self.sendLine(", ".join(self.factory.channels.keys()).encode("utf-8"))
-            else:
-                self.sendLine("! Arguments not understood")
-
-        elif line.startswith("/tell"): # privately say something to someone else
-            for c in self.factory.channels[self.channel]:
-                if c.name in line.split(" ")[1].split(","):
-                    c.sendLine("[{} # {}".format(self.name, " ".join(line.split(" ")[2:])))
-                    if c.away:
-                        self.sendLine("! {} is currently away. They might not see the message you just sent".format(line.split(" ")[1].split(",")))
-
-        elif line.startswith("/join"):
-            newchannel = line.split(" ")[1]
-            if newchannel not in self.factory.channels.keys():
-                self.factory.channels[newchannel] = set()
-            self.factory.channels[self.channel].remove(self)
-            self.factory.channels[newchannel].add(self)
-            self.broadcast("! {} has left {}".format(self.name, self.channel), noEcho=False)
-            self.channel = newchannel
-            self.broadcast("! {} has joined {}".format(self.name, self.channel), noEcho=False)
-
-        elif line.startswith("/channel"):
-            if len(line.split(" ")) == 1:
-                self.sendLine("! You are currently in {}".format(self.channel))
-            else:
-                self.sendLine("! Invalid Arguments")
-
-        elif line.startswith("/away"):
-            if self.away:
-                self.away = True
-                self.broadcast("! {} is now away".format(self.name), noEcho=True)
-            else:
-                self.away = False
-                self.broadcast("! {} is now back".format(self.name), noEcho=True)
-
-        elif line.startswith("/rules"):
-            self.sendLine(cfg.rules)
-
-        elif line.startswith("/exit"):
-            self.transport.loseConnection()
-
-        elif line.startswith("/help"): # help command
-            self.sendLine("Help text:")
-            self.sendLine("/name <name>         : sets your name")
-            self.sendLine("/rules               : prints rules") # set in config.py
-            self.sendLine("/me <action>         : says you did something")
-            self.sendLine("/list                : list all users in channel")
-            self.sendLine("/tell <name> <msg>   : tell username something")
-            self.sendLine("/list channels       : list all channels")
-            self.sendLine("/join <name>         : join a channel")
-            self.sendLine("/whoami              : tells you what your name is")
-            self.sendLine("/away                : sets status as away")
-            self.sendLine("/exit                : terminates your connection")
-
-        elif line.startswith("/"): # catch-all for malformed or unrecoignized commands
-            self.sendLine("! Command not recoignized type /help for help")
-
+                    self.echo("! must specifiy a name to change")
+                    return
+            elif message.startswith("whoami"):
+                self.echo("! you are {}".format(self.peer.name))
+                return
+            elif message.startswith("me"):
+                self.broadcast("*{} {}".format(self.peer.name, " ".join(message.split(" ")[1:])))
+                return
+            elif message.startswith("tell"):
+                tokens = message.split(" ")[1:]
+                for peer in peers:
+                    if peer.name == tokens[0]:
+                        peer.transport.write(" ".join(tokens[1:]))
+                        return
+                self.echo("! {} not found on server".join(tokens[0]))
+                return
+            elif message.startswith("exit"):
+                self.peer.transport.close()
         else:
-            if cfg.echo:
-                self.broadcast("{0}{1}{2} {3}".format(cfg.antecedent, self.name, cfg.postscript, line), noEcho=False) # otherwise just broadcast it
-            else:
-                self.broadcast("{0}{1}{2} {3}".format(cfg.antecedent, self.name, cfg.postscript, line)) # otherwise just broadcast it
+            self.broadcast("<{}> {}".format(self.peer.name, message))
 
-class ServerFactory(protocol.Factory):
-    def __init__(self):
-        self.channels = {}
-        self.channels[cfg.defaultChannel] = set()
+        def connection_lost(self, exc):
+            self.broadcast("! {} has left the server".format(self.peer.name))
+            print("Connection from {} lost: {}".format(self.peername, exc))
+            del peers[peers.index(self.peer)]
 
-    def buildProtocol(self, addr):
-        return ChatProtocol(self)
 
-endpoints.serverFromString(reactor, "tcp:{}".format(cfg.port)).listen(ServerFactory())
-reactor.run()
+loop = asyncio.get_event_loop()
+# Each client connection will create a new protocol instance
+coro = loop.create_server(ChatServerProtocol, '127.0.0.1', 1234)
+server = loop.run_until_complete(coro)
+
+# Serve requests until Ctrl+C is pressed
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+# Close the server
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
